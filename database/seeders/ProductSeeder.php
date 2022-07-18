17d5aba2f791821bfdc1d121390dedf14cf4b485
Andrey Mishchenko <msav@msav.ru>
2022-07-18 09:09:42 +0300

    feature/CU-2rb3a2z_Catalog-models Класс наполнения каталога товаров

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+/**
+ * Database\Seeders\ProductSeeder
+ *
+ * @Class ProductSeeder Наполнение каталога товаров.
+ * @package Database\Seeders
+ */
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        //
+    }
+}
